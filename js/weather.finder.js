4d757c1dbc4d841522b94ddc195f5c81e3d1c0b7
Travis Gomez <frank.travis.gomez@gmail.com>
2023-03-14 15:55:07 -0500

    5 day forcast complete

--- js/weather.finder.js
+++ js/weather.finder.js
@@ -58,19 +58,49 @@ function geoCodeBuildWeather(searchString) {
 
 
                 html += `<h5>Current humidity: ${parseInt(data.main.humidity)}</h5>`;
-
+            $("#day1").html(html);
                 // $("#weatherBody").html(html);
-            //Second request inside of first request: goal is to have current conditions built and stored inside of variable then continue building more html next with forecast data
+            //Second request inside-of first request: goal is to have current conditions built and stored inside of variable then continue building more html next with forecast data
 
                 $.get(`https://api.openweathermap.org/data/2.5/forecast?lat=${results[1]}&lon=${results[0]}&appid=${openWeatherKey}&units=imperial`).done(function (data) {
                 console.log(data);
-
+            var html4cast="";
                 //What shape is my object? How do I build more html here?
+                //  for(let i=0; i<list.length; i+=0)
+                    html4cast += `<h4>Current location: ${data.city.name}</h4>`
+                    html4cast += `<h4>Date: ${data.list[2].dt_txt}</h4>`
+                    html4cast += `<h4>Future weather: ${data.list[2].weather[0].description}</h4>`
+                    html4cast += `<h5>Future average wind speed: ${parseInt(data.list[2].wind.speed)} knots</h5>`
+                    html4cast += `<h5>Future Temperature:${parseInt(data.list[2].main.temp)} &deg;</h5>`
+                    html4cast += `<h5>Future humidity: ${parseInt(data.list[2].main.humidity)}</h5>`
+                    $("#day2").html(html4cast);
+            var html4pt2="";
 
-                    html += `<h4>Current location: ${data.city.name}</h4>`
-                    html += `<div>${data.list}</div>`
-                    html += `<div>example div</div>`
-                    $("#weatherBody").html(html);
+                    html4pt2 += `<h4>Current location: ${data.city.name}</h4>`
+                    html4pt2 += `<h4>Date: ${data.list[9].dt_txt}</h4>`
+                    html4pt2 += `<h4>Future weather: ${data.list[9].weather[0].description}</h4>`
+                    html4pt2 += `<h5>Future average wind speed: ${parseInt(data.list[9].wind.speed)} knots</h5>`
+                    html4pt2 += `<h5>Future Temperature:${parseInt(data.list[9].main.temp)} &deg;</h5>`
+                    html4pt2 += `<h5>Future humidity:${parseInt(data.list[9].main.humidity)}</h5>`
+                    $("#day3").html(html4pt2);
+            var html4pt3="";
+
+                    html4pt3 += `<h4>Current location: ${data.city.name}</h4>`
+                    html4pt3 += `<h4>Date: ${data.list[17].dt_txt}</h4>`
+                    html4pt3 += `<h4>Future weather: ${data.list[17].weather[0].description}</h4>`
+                    html4pt3 += `<h5>Future average wind speed: ${parseInt(data.list[17].wind.speed)} knots</h5>`
+                    html4pt3 += `<h5>Future Temperature: ${parseInt(data.list[17].main.temp)} &deg;</h5>`
+                    html4pt3 += `<h5>Future humidity: ${parseInt(data.list[17].main.humidity)}</h5>`
+                    $("#day4").html(html4pt3);
+            var html4pt4="";
+
+                    html4pt4 += `<h4>Current location: ${data.city.name}</h4>`
+                    html4pt4 += `<h4>Date: ${data.list[25].dt_txt}</h4>`
+                    html4pt4 += `<h4>Future weather: ${data.list[25].weather[0].description}</h4>`
+                    html4pt4 += `<h5>Future average wind speed: ${parseInt(data.list[25].wind.speed)} knots</h5>`
+                    html4pt4 += `<h5>Future Temperature: ${parseInt(data.list[25].main.temp)} &deg;</h5>`
+                    html4pt4 += `<h5>Future humidity: ${parseInt(data.list[25].main.humidity)}</h5>`
+                    $("#day5").html(html4pt4);
             })
             }
         )
