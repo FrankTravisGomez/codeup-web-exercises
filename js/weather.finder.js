b91a3f6320bac6bb5ef23aae61baa2853f90d47b
Travis Gomez <frank.travis.gomez@gmail.com>
2023-03-15 10:07:28 -0500

    update bootstrap

--- js/weather.finder.js
+++ js/weather.finder.js
@@ -1,47 +1,15 @@
 
 
 
-//29.622157157950287, -98.50589962996946
-// console.log("more info");
-// console.log(`https://api.openweathermap.org/data/2.5/weather?lat=${29.622157157950287}&lon=${-98.50589962996946}&appid=${openWeatherKey}`);
-//
-//
-// console.log(`https://api.openweathermap.org/data/2.5/weather?lat=${29.622157157950287}&lon=${-98.50589962996946}&appid=${openWeatherKey}&units=imperial`);
-
-//stuff on screen
-
 console.log("is it working");
-// function weatherDay(string){
-//     let html="";
-//     geocode(string,mapboxKey).then(function(results){
-//         $.ajax(`https://api.openweathermap.org/data/2.5/forecast?lat=${results[1]}&lon=${results[0]}&appid=${openWeatherKey}&units=imperial`).done(function(data){
-//             html+= `<h4>aktuelle Standort: ${data.name}</h4>`;
-//     console.log(data);
-//             html += `<h4>aktuelle Wetter: ${data.weather[0].description}</h4>`;
-//
-//
-//             html += `<h5>aktuelle durchschnittliche Windgeschwindigkeit: ${parseInt(data.wind.speed)} knots</h5>`;
-//
-//             html += `<h5>aktuelle Temperatur: ${parseInt(data.main.temp)} &deg;</h5>`;
-//
-//
-//             html += `<h5>aktuelle  Luftfeuchtigkeit: ${parseInt(data.main.humidity)}</h5>`;
-//
-//             $("#weatherBody").html(html);
-//         })
-//     })
-// }
-// $("#btn1").click(function(e){
-//     e.preventDefault();
-//     weatherDay($("#searchInput").val());
-// })
 
 // $.ajax(`api.openweathermap.org/data/2.5/forecast?lat=${results[1]}&lon=${results[0]}&appid=${openWeatherKey}&units=imperial`)
 function geoCodeBuildWeather(searchString) {
     let html = "";
     geocode(searchString, mapboxKey).then(function (results) {
 
-
+console.log(results)
+    console.log(`https://api.openweathermap.org/data/2.5/weather?lat=${results[1]}&lon=${results[0]}&appid=${openWeatherKey}&units=imperial`)
         //my home cord: 29.623148749882247, -98.50846661345147
         $.get(`https://api.openweathermap.org/data/2.5/weather?lat=${results[1]}&lon=${results[0]}&appid=${openWeatherKey}&units=imperial`).done(function (data) {
                 console.log(data);
@@ -118,4 +86,38 @@ $("#btn1").click(function(e){
     e.preventDefault();
     geoCodeBuildWeather($("#searchInput").val());
 })
+// let myToken = 'pk.eyJ1IjoidHJhdmlzY29kZXVwIiwiYSI6ImNsZjJzdm5pNDBsaGgzcmxoYWNueHlkcnQifQ.0xOFa6MG7IZFECJVLYQOZQ';
 
+mapboxgl.accessToken = mapboxKey;
+
+//javascript map cords 29.626185451652596, -98.4949673897373
+const map = new mapboxgl.Map(
+    {
+        container: 'map', // container ID
+        style: 'mapbox://styles/mapbox/dark-v11', //  URL to make it look cool
+        center: [-98.4949673897373, 29.626185451652596], // positioning lng, lat goes inside []
+        zoom: 10,
+    }
+);
+let testArray= [{
+    name: "raising cane's",
+    location:[1, 0],
+    review: 'best chicken on this side of town',
+},{
+    name: "trilogy pizza",
+    location: [1, 0],
+    review:"almost like chicago but not as good",
+},{
+    name: "Boiler House at the Pearl",
+    location: [1, 0],
+    review: "fany place for them fancy people",
+}
+];
+testArray.forEach(place => {
+    let marker = new mapboxgl.Marker()
+        .setLngLat(place.location)
+        .addTo(map);
+    let popup = new mapboxgl.Popup()
+    popup.setHTML(`<p>${place.name} - ${place.review}</p>`)
+    marker.setPopup(popup);
+});
