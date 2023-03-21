7ac68dc338db1980991e08eba0fce5cd5b5f7f21
Travis Gomez <frank.travis.gomez@gmail.com>
2023-03-20 20:09:52 -0500

    complete

--- js/promises.js
+++ js/promises.js
@@ -3,23 +3,48 @@ console.log("is it working")
 // 1)Create a file named promises.js inside-of your js directory
 // and link it to an HTML file of your choice. COMPLETE
 
-// 2)Generate a Personal Access Token for the GitHub API. COMPLETE
 
-// 3)Create a function that accepts a GitHub username,
-// and returns a promise that resolves returning just the date of the last commit that user made.
-// Reference the github api documentation to achieve this.
 // fetch('url/token', {token})
 //  .then(res => res.json() )
 //     .then(data => {
 //        console.log(data)
 //     })
 
-//the code underneath is for a fetch request from my github to pull my commit info
-fetch(`https://api.github.com/users/FrankTravisGomez/events/public`, {Authorization: githubKey}).then( res => res.json())
+
+// 2)Generate a Personal Access Token for the GitHub API. COMPLETE
+//the code underneath is for a fetch request from my git-hub to pull my commit info
+// .json object looks like javascript object, but will take fetch response and turn it into a json object
+fetch(`https://api.github.com/users/FrankTravisGomez/events/public`, {Authorization: githubKey}).then( res => res.json()).catch(error => console.error(error.message))
+    .then(events => events.filter(event => event.type === "PushEvent"))
+    .then(events => events[0].created_at)
     .then(function(data){
 
         console.log(data)
     })
+// 3)Create a function that accepts a GitHub username,
+// and returns a promise that resolves returning just the date of the last commit that user made.
+// Reference the git-hub api documentation to achieve this.
+// function gitLast(user){
+// }
+// function gitLast(user){
+//     fetch(`https://api.github.com/users/FrankTravisGomez/events/public`, {Authorization: githubKey}).then(res => res.json())
+//         .then(events => events.filter(event => event.type === 'PushEvent')[29]);
+//     console.log(user)
+// }
+
+fetch(`https://api.github.com/users/FrankTravisGomez/events/public`, {Authorization: githubKey}).then( res => res.json()).catch(error => console.error(error.message))
+    .then(function(data){
+        // return new Promise(info);{
+        //     return pushEvent
+        // }
+        console.log(data)
+    })
+
+
+// 1)Write a function named wait that accepts a number as a parameter,
+// and returns a promise that resolves after the passed number of milliseconds. COMPLETE
+// As a bonus make sure the promise resolves with the milliseconds in return,
+// so you can make the console log message more dynamic. COMPLETE
 // function wait and a return for a promise to resolve and reject using Math.random with a timer set to 3000
 function wait() {
     return new Promise((resolve, reject) => {
